1160981a13491ad1603649d6dcf55d5a5bcabf92
CWU-Bjorn <AddingtonB@cwu.edu>
2026-05-16 22:23:36 -0700

    Add files via upload
    Initial deliverables for week one. Creation of the player class, implementation of a new database design and system with SQLite, connection of the database to the player and the ability to password protect the different save slots. This works as a good start, but the real work will happen once we connect this to the GUI and the other implementations.

---
+++ CS380Project/src/DatabaseConnection.java
@@ -0,0 +1,88 @@
+/**
+ * Import of the necessary stuff, I just imported all of the libraries so this would probably need to change later for efficiency
+ */
+
+import java.io.*;
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    /**
+     *Method that connects to the database by passing the url of the file into it
+     */
+       private static Connection connect() throws SQLException{
+           return DriverManager.getConnection("jdbc:sqlite:380DatabaseSQLite.db");
+        }
+
+    /**
+     * Finds the table within the database
+     */
+    private static String findsTable(int saveslotRotation){
+           if(saveslotRotation == 1){
+               return "SaveFile1";
+           } else if (saveslotRotation == 2) {
+               return "SaveFile2";
+           } else if (saveslotRotation == 3) {
+               return "SaveFile3";
+           } else {
+               throw new IllegalArgumentException("There is no saveslot with this designation, enter 1 , 2 , 3 to choose a save.");
+           }
+}
+
+    /**
+     *Finds the primary key of the table
+     */
+    private static String findsPrimaryKey(int saveslotRotation){
+        if(saveslotRotation == 1){
+            return "Save1";
+        } else if (saveslotRotation == 2) {
+            return "Save2";
+        } else if (saveslotRotation == 3) {
+            return "Save3";
+        } else {
+            throw new IllegalArgumentException("There is no saveslot with this designation, enter 1 , 2 , 3 to choose a save.");
+        }
+    }
+
+    /**
+     *Runs SQL query to fetch everything from the located save slot. It does this by mathcing the table with the values in the if statement
+     * wrapped in a try/catch statement. In the future this section will likely look a lot different.
+     */
+    public static Player loadPlayer(int saveslotRotation){
+           String foundTable = findsTable(saveslotRotation);
+           String foundPrimaryKey = findsPrimaryKey(saveslotRotation);
+           String executableSQL = "SELECT * FROM " + foundTable + " WHERE " +foundPrimaryKey + " = ?";
+
+            try(
+                    Connection connection = connect();
+                    PreparedStatement statement = connection.prepareStatement(executableSQL);
+                    ){
+
+                statement.setInt(1, saveslotRotation);
+
+                ResultSet resultSet = statement.executeQuery();
+
+                if(resultSet.next()){
+                    return new Player(
+                            saveslotRotation,
+                            resultSet.getString("Pname"),
+                            resultSet.getString("PWord"),
+                            resultSet.getString("Item1"),
+                            resultSet.getString("Item2"),
+                            resultSet.getString("Item3"),
+                            resultSet.getString("Item4"),
+                            resultSet.getString("Item5"),
+                            resultSet.getString("Item6"),
+                            resultSet.getInt("Currency"),
+                            resultSet.getInt("NumberObstaclesDone")
+                    );
+                }
+            }catch (SQLException e){
+                System.out.println("Trouble with the database, unable to load it");
+
+            }
+            return null;
+    }
+        }
+
+
---
+++ CS380Project/src/Main.java
@@ -0,0 +1,46 @@
+/**
+ * Imports, as with the other files this could probably be cleaned up
+ */
+
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+/**
+ * Declaration of main argument
+ */
+public class Main {
+    public static void main(String[] args) {
+/**
+ *This prompts the user to enter a save slot so they can start the game
+ */
+        int choosenSlot = 0;
+        Scanner userVar = new Scanner(System.in);
+        System.out.print("Please enter the save slot you wish to use: ");
+        choosenSlot = userVar.nextInt();
+        userVar.nextLine();
+        /**
+         * Uses what the player enters to connect to the database and run code in the DatabaseConnection.java file
+         */
+        Player clientSideUser = DatabaseConnection.loadPlayer(choosenSlot);
+
+        /**
+         * Same as the user prompt above except it wants the password the is unique to each save. Even if two saves have the same password the save must first be
+         * designated and because of that there will never be conflict as the save file number is unique for each save slot and thus the password will also be unique.
+         */
+        String userPassword = "";
+        System.out.println("Please enter your password for this save: ");
+        userPassword = userVar.nextLine();
+
+        /**
+         * This uses the method in the player.java file to call and check it the String entered is actually the password that is being looked for.
+         */
+        if(clientSideUser.isPasswordCorrect(userPassword)){
+            System.out.println("Welcome back! You are now logged in!");
+            System.out.println(clientSideUser);
+        }else{
+            System.out.println("Password does not match the correct password");
+        }
+
+    }
+}
