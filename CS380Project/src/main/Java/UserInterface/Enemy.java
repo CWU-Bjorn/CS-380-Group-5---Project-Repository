8898950658251e0a143c5f264b740a4b09f42468
NBNStacked <153414505+NBNStacked@users.noreply.github.com>
2026-06-02 16:52:05 -0700

    Enemy Attack Event to GUI Connection
    +Added lines to GameplayController.Java to allow connection between Enemy Attack event to GUI

--- CS380Project/src/main/Java/UserInterface/Enemy.java
+++ CS380Project/src/main/Java/UserInterface/Enemy.java
@@ -14,21 +14,59 @@ public class Enemy {
         this.damage = damage;
     }
 
+    public String getName() {return name;}
+    public int getHealth() {return health;}
+    public int getDamage() {return damage;}
+
     public String toString() {
         return name
                 + "\n" + health
                 + "\n" + damage;
     }
 
-    public void enemyAttack(Player player){
+    public String enemyAttack(String action, Player player){
 
-        System.out.println("===ENEMY ENCOUNTERED!===");
+        StringBuilder actionText = new StringBuilder();
 
         Random randChance = new Random();
 
-        Scanner userInp = new Scanner(System.in);
-        String userChoice = "";
+        switch(action.toLowerCase()){
+            case "attack":
+                int chance = randChance.nextInt(10);
+                if (chance >=5){
+                    health -= 2;
+                }
+                else{
+                    actionText.append("No Luck! You've Missed!\n");
+                }
+                break;
+            case "flee":
+                return "You've decided to flee!";
+            case "shield":
+                actionText.append("You decided to Shield\n");
+                break;
+            case "heal":
+                actionText.append("You decided to Heal\n");
+                break;
 
+        }
+
+        if(health > 0) {
+            int enemyChance = randChance.nextInt(10);
+            if (enemyChance >= 5) {
+                actionText.append("Enemy has struck you!");
+                player.setPlayerHP(damage);
+            } else {
+                actionText.append("Enemy Missed!");
+            }
+        }else{
+            actionText.append("Enemy Has Been Defeated!");
+        }
+        return actionText.toString();
+
+
+
+        /*
         while (health != 0){
 
             if(health == 0){
@@ -40,6 +78,14 @@ public class Enemy {
             System.out.print("Enemy is preparing to attack... What will you do?");
             userChoice = userInp.nextLine();
 
+            if(userChoice.equalsIgnoreCase("run away")){
+                System.out.println("You decided to run away");
+                break;
+            }
+
+            if(userChoice.equalsIgnoreCase("shield")){
+                System.out.println("You decided to shield");
+            }
 
             if (userChoice.equalsIgnoreCase("attack")){
                 int chance = randChance.nextInt(10);
@@ -52,16 +98,9 @@ public class Enemy {
 
             }
 
-            int enemyChance =  randChance.nextInt(10);
-                if(enemyChance >=5){
-                    System.out.println("Enemy has struck you!");
-                    player.setPlayerHP(damage);
-                }else{
-                    System.out.println("Enemy Missed!");
-                }
-
-
         }
+        */
+
     }
 
 }
