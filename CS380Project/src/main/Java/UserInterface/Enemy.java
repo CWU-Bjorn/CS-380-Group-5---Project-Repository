ec56eb6e2dbf150f5c7e28d23af440cc0bd211be
NBNStacked <153414505+NBNStacked@users.noreply.github.com>
2026-06-03 23:10:14 -0700

    Comments to Code

--- CS380Project/src/main/Java/UserInterface/Enemy.java
+++ CS380Project/src/main/Java/UserInterface/Enemy.java
@@ -3,33 +3,57 @@ package UserInterface;
 import java.util.Scanner;
 import java.util.Random;
 
+/*Enemy Class
+Sets up an enemy and contains an attack event for interactions
+between the player
+ */
+
 public class Enemy {
+
+    //Enemy Attributes
     private String name = "Dummy name";
     private int health = 20;
     private int damage = -2;
 
+    //Constructor
     public Enemy(String name, int health, int damage) {
         this.name = name;
         this.health = health;
         this.damage = damage;
     }
 
+    //Getter methods to retrieve enemy attributes
     public String getName() {return name;}
     public int getHealth() {return health;}
     public int getDamage() {return damage;}
 
+    //toString Method to combine attributes into a string variable
     public String toString() {
         return name
                 + "\n" + health
                 + "\n" + damage;
     }
 
+    /*EnemyAttack attribute
+      Places the player an enemy into an attack event
+      Uses the player and action as parameters
+     */
     public String enemyAttack(String action, Player player){
 
+        //actionText. StringBuilder variable to display action taken by players
         StringBuilder actionText = new StringBuilder();
+
+        //Random randChance to determine attack success for player
         Random randChance = new Random();
 
+        /* Switch block
+           Determines action based on string given by parameter
+           Using GUI buttons to hold values to then pass on to the parameters
+         */
+
         switch(action.toLowerCase()){
+
+            //Attack Case. Uses Random value for attack chance as well as scales damage
             case "attack":
                 int chance = randChance.nextInt(10);
                 if (chance >=5){
@@ -42,11 +66,17 @@ public class Enemy {
                     actionText.append("No Luck! You've Missed!\n");
                 }
                 break;
+
+            //Flee. Simply Exits the interaction
             case "flee":
                 return "You've decided to flee!";
+
+            //Shield Block. Prevents damage given to player
             case "shield":
                 actionText.append("You decided to Shield\n");
                 return actionText.toString();
+
+            //Heal Block. Heals the player when executed
             case "heal":
                 actionText.append("You decided to Heal\n");
                 player.setPlayerHP(5);
@@ -57,6 +87,10 @@ public class Enemy {
 
         }
 
+        /*If-else statements.
+          Checks enemy health. If enemy still has health, enemy may attack
+          Also checks to see if enemy is vanquished
+         */
         if(health > 0) {
             int enemyChance = randChance.nextInt(10);
             if (enemyChance >= 5) {
--- CS380Project/src/main/Java/UserInterface/Puzzle.java
+++ CS380Project/src/main/Java/UserInterface/Puzzle.java
@@ -2,31 +2,55 @@ package UserInterface;
 import java.util.*;
 import java.io.*;
 
+/*Puzzle Class
+  Contains attributes for creating a puzzle
+  as well as manages attributes of a puzzle
+ */
 class Puzzle {
 
+    /*Puzzle Attributes
+      Checks for puzzle completion as well as
+      Stores word to guess and a hidden version of the word
+      to manipulate
+     */
     private boolean puzzleStatus = false;                                   // Flag for successful puzzle completion
     private String wordToGuess;
     private StringBuilder wordHidden = new StringBuilder();
 
+    //Getter Methods to retrieve attributes
     public String getHiddenWord(){ return wordHidden.toString(); }
     public String getWordToGuess(){ return wordToGuess;}
     public boolean getPuzzleStatus(){ return puzzleStatus; }
 
+    //Puzzle Event method to place the player in a player event
     public void puzzleEvent() {
+
+        //Statement to confirm initiation
         System.out.println("Puzzle Started");
+
+        //String variable line. Used to store words extracted from the wordbank file
         String line;
+
+        //Stores the various words from the wordbank file onto the arraylist.
         List<String> wordList = new ArrayList<String>();
 
+        /*Try-Catch block. Determines existence of file to extract lines
+          If not found, throw exception
+          If found, place words into line variable, then transfer to arrayList
+         */
         try(InputStream is = getClass().getResourceAsStream("/puzzle_wordbank.txt")){
 
             System.out.println("InputStream: " + is);
 
+            //File Reader using BufferedReader
             BufferedReader fileBuff = new BufferedReader(new InputStreamReader(is));
 
+            //While contents exist in file, extract onto line variable, then trim and place on arrayList
             while((line = fileBuff.readLine()) !=null){
                 wordList.add(line.trim());
             }
 
+            //Count list size
             System.out.println("wordList: " + wordList.size());
         }catch(IOException | NullPointerException e){
             System.out.println("File not found...");
@@ -35,12 +59,23 @@ class Puzzle {
             return;
         }
 
+        //Random randChoice. Selects random word to make the player guess
         Random randChoice = new Random();
+
+        //Word guess variable grabbed from arrayList, then set on wordToGuess variable
         wordToGuess = wordList.get(randChoice.nextInt(wordList.size()));
+
+        /*Based on the size of the wordToGuess variable, build the hidden word variable
+          Using StringBuilder, successful letter guesses replace the "hidden" portions
+          to the correct letter
+         */
         wordHidden = new StringBuilder("_".repeat(wordToGuess.length()));
 
     }
 
+    /*GuessLetter method. Allows for the player to guess
+      on a letter-by-letter case. Will update on successful guesses
+     */
     public boolean guessLetter(char letter) {
         letter = Character.toLowerCase(letter);
 
